ee1d1101bc5083fdfb6d6ea80cfe959207979d99
adrianoraya <adrian.oraya@stratpoint.com>
2024-10-30 15:37:14 +0800

    fix(email-service): fix authorization implementation

--- packages/email-service/src/app.module.ts
+++ packages/email-service/src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '../../user-service/src/auth/auth.module';
 import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [
+        () => ({
+          JWT_SECRET: process.env.JWT_SECRET,
+        }),
+      ],
     }),
     EmailModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
