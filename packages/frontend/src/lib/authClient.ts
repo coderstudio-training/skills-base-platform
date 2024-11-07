59f6bffa9c7558c33a492a4109d8f963aabcd90a
Vz-3 <vondc6201@gmail.com>
2024-11-07 16:50:18 +0800

    feat(frontend): delete third party & hooks

--- packages/frontend/src/lib/authClient.ts
+++
@@ -1,6 +0,0 @@
-import fetchData, { RequestConfig } from './httpClient';
-
-export async function fetchWithAuth<T>(config: RequestConfig, token: string): Promise<T> {
-  const authHeaders = { Authorization: `Bearer ${token}` };
-  return fetchData<T>({ ...config, headers: { ...config.headers, ...authHeaders } });
-}
