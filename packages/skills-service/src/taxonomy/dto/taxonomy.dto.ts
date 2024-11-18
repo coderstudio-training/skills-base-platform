449f5a2bbcc98a818e45ba8d1f2b9297ec9503d9
vonderwincaibigan <vonderwin.caibigan@stratpoint.com>
2024-11-18 11:44:50 +0800

    feat(skills-service): add soft skill dto

--- packages/skills-service/src/taxonomy/dto/taxonomy.dto.ts
+++ packages/skills-service/src/taxonomy/dto/taxonomy.dto.ts
@@ -3,6 +3,7 @@ import { Type } from 'class-transformer';
 import {
   IsArray,
   IsNotEmpty,
+  IsNotEmptyObject,
   IsObject,
   IsOptional,
   IsString,
@@ -57,8 +58,53 @@ export class T_TaxonomyDTO extends BaseDto {
   rangeOfApplication?: string[];
 }
 
+export class S_TaxonomyDTO extends BaseDto {
+  @IsString()
+  @IsNotEmpty({ message: 'document id must not be empty!' })
+  docId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  docRevisionId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  docTitle!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  title!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  category!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description!: string;
+
+  @IsObject()
+  @IsNotEmptyObject()
+  profiencyDescription!: Record<string, string[]>;
+
+  @IsObject()
+  @IsNotEmptyObject()
+  benchmark!: Record<string, string[]>;
+
+  // Reserved for the collection name.
+  @IsString()
+  @IsNotEmpty()
+  businessUnit!: string;
+}
+
 export class BulkUpsertTTaxonomyDTO {
   @ValidateNested({ each: true })
   @Type(() => T_TaxonomyDTO)
   data!: T_TaxonomyDTO[];
+}
+
+export class BulkUpsertSTaxonomyDTO {
+  @ValidateNested({ each: true })
+  @Type(() => T_TaxonomyDTO)
+  data!: S_TaxonomyDTO[];
 }
