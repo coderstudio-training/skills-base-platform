11b915e22869939cebc1f8ce77a9ab480caee041
vonderwincaibigan <vonderwin.caibigan@stratpoint.com>
2024-12-15 02:44:02 +0800

    feat(frontend): update admin & manager loading

--- packages/frontend/src/app/dashboard/manager/layout.tsx
+++ packages/frontend/src/app/dashboard/manager/layout.tsx
@@ -1,13 +1,10 @@
-import { ErrorBoundary } from '@/components/Dashboard/ErrorBoundary';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Suspense } from 'react';
 
 export default function ManagerLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <ErrorBoundary>
-        <Suspense fallback={<Skeleton className="w-full h-screen" />}>{children}</Suspense>
-      </ErrorBoundary>
+      <Suspense fallback={<Skeleton className="w-full h-screen" />}>{children}</Suspense>
     </div>
   );
 }
--- packages/frontend/src/app/dashboard/manager/loading.tsx
+++ packages/frontend/src/app/dashboard/manager/loading.tsx
@@ -1,3 +1,4 @@
+import LoadingCard from '@/components/Dashboard/components/Cards/LoadingCard';
 import LoadingHeader from '@/components/Dashboard/components/Header/LoadingHeader';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Loader2 } from 'lucide-react';
@@ -8,9 +9,9 @@ export default function ManagerLoading() {
       <LoadingHeader />
       <div>
         <div className="h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground grid w-full grid-cols-5">
-          <Skeleton className="bg-gray-300 text-transparent inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium">
+          <div className="bg-white text-transparent inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium">
             <Skeleton className="bg-gray-200 text-transparent rounded-sm">Tab List 1 </Skeleton>
-          </Skeleton>
+          </div>
           <div className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium text-transparent">
             <Skeleton className="bg-gray-200 text-transparent rounded-sm">Tab List 2 </Skeleton>
           </div>
@@ -27,60 +28,32 @@ export default function ManagerLoading() {
         <div className="mt-2 space-y-4">
           <div className="space-y-4">
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-              <Skeleton className="bg-gray-200 rounded-xl border shadow bg-card">
-                <div className="flex flex-col space-y-1.5 p-6">
-                  <div className="flex items-center justify-between">
-                    <div className="space-y-1.5">
-                      <Skeleton className="bg-gray-300 rounded-md text-transparent font-semibold tracking-tight leading-none">
-                        Card ****
-                      </Skeleton>
-                    </div>
-                    <div className="ml-2"></div>
-                  </div>
-                </div>
-                <div className="p-6 pt-0">
+              <LoadingCard
+                bodyChildren={
                   <Skeleton className="bg-gray-300 text-2xl rounded-md font-bold text-transparent">
                     ****
                   </Skeleton>
-                </div>
-              </Skeleton>
-              <Skeleton className="bg-gray-200 rounded-xl border shadow bg-card">
-                <div className="flex flex-col space-y-1.5 p-6">
-                  <div className="flex items-center justify-between">
-                    <div className="space-y-1.5">
-                      <Skeleton className="bg-gray-300 rounded-md text-transparent font-semibold tracking-tight leading-none">
-                        Card ****
-                      </Skeleton>
-                    </div>
-                    <div className="ml-2"></div>
-                  </div>
-                </div>
-                <div className="p-6 pt-0">
+                }
+              />
+              <LoadingCard
+                bodyChildren={
                   <Skeleton className="bg-gray-300 text-2xl rounded-md font-bold text-transparent">
                     ****
                   </Skeleton>
-                </div>
-              </Skeleton>
-              <Skeleton className="bg-gray-200 rounded-xl border shadow bg-card">
-                <div className="flex flex-col space-y-1.5 p-6">
-                  <div className="flex items-center justify-between">
-                    <div className="space-y-1.5">
-                      <Skeleton className="bg-gray-300 rounded-md text-transparent font-semibold tracking-tight leading-none">
-                        Card ****
-                      </Skeleton>
-                    </div>
-                    <div className="ml-2"></div>
+                }
+              />
+              <LoadingCard
+                bodyChildren={
+                  <div>
+                    <Skeleton className="bg-gray-300 text-2xl rounded-md font-bold text-transparent">
+                      ****
+                    </Skeleton>
+                    <Skeleton className="bg-gray-300 text-xs text-transparent mt-1 w-1/4 rounded-md">
+                      ****
+                    </Skeleton>
                   </div>
-                </div>
-                <div className="p-6 pt-0">
-                  <Skeleton className="bg-gray-300 text-2xl rounded-md font-bold text-transparent">
-                    ****
-                  </Skeleton>
-                  <Skeleton className="bg-gray-300 text-xs text-transparent mt-1 w-1/4 rounded-md">
-                    ****
-                  </Skeleton>
-                </div>
-              </Skeleton>
+                }
+              />
             </div>
             <div className="grid gap-4 md:grid-cols-2">
               <Skeleton className="bg-gray-200 rounded-xl border shadow bg-card">
@@ -102,9 +75,7 @@ export default function ManagerLoading() {
                       <div className="relative w-full h-full max-h-[400px] max-w-[516px]">
                         <div className="flex flex-col items-center justify-center align-middle h-full w-full gap-2">
                           <Loader2 className="h-8 w-8 animate-spin" />
-                          <Skeleton className="text-sm text-muted-foreground">
-                            Loading content...
-                          </Skeleton>
+                          <div className="text-sm text-muted-foreground">Loading content...</div>
                         </div>
                       </div>
                     </div>
