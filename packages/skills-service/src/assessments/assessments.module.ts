ac273443dacabf073a6a041704fb307b8d5f059f
vonderwincaibigan <vonderwin.caibigan@stratpoint.com>
2024-12-17 16:34:37 +0800

    fix(skills-service): remove permission and re-skills-matrix import in app module

--- packages/skills-service/src/assessments/assessments.module.ts
+++ packages/skills-service/src/assessments/assessments.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CacheModule } from '@skills-base/shared';
 import { AssessmentsController } from './controllers/assessments.controller';
 import { PerformanceController } from './controllers/computation.controller';
-import { SkillsMatrixController } from './controllers/re-skills-matrix.controller';
 import { SkillMatrixController } from './controllers/skill-matrix.controller';
 import { AssessmentsService } from './services/assessments.service';
 import { PerformanceService } from './services/computation.service';
@@ -23,7 +22,6 @@ import { SkillsMatrixRepository } from './skills-matrix.repository';
   controllers: [
     AssessmentsController,
     PerformanceController,
-    SkillsMatrixController,
     SkillMatrixController,
   ],
   providers: [
