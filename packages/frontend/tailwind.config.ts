25e8437e19b9b82efcdb25cb7c445677ce8ab468
vonderwincaibigan <vonderwin.caibigan@stratpoint.com>
2024-12-19 14:48:43 +0800

    feat(frontend): add scrollbar & additional animations for tailwindcss

--- packages/frontend/tailwind.config.ts
+++ packages/frontend/tailwind.config.ts
@@ -65,12 +65,26 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 },
         },
+        fadeInOut: {
+          '0%': { opacity: '0', transform: 'scale(0.8)' },
+          '50%': { opacity: '1', transform: 'scale(1.2)' },
+          '100%': { opacity: '0', transform: 'scale(0.8)' },
+        },
+        scaleIn: {
+          '0%': { transform: 'scale(0)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        fadeInOut: 'fadeInOut 0.5s ease-in-out',
+        scaleIn: 'scaleIn 0.3s ease-out',
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
+  variants: {
+    scrollbar: ['rounded'],
+  },
 };
